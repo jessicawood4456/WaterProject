c8334ca78882422a7a38c53309ac121ff7ec7acc
Jessica Lasson <jessicawood4456@gmail.com>
2025-03-18 13:34:39 -0600

    Initial setup and connection to the database

---
+++ backend/WaterProject/WaterProject/Controllers/WaterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.Data;
+
+namespace WaterProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _context;
+        
+        public WaterController(WaterDbContext temp)
+        {
+            _context = temp;
+        }
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> GetProjects()
+        {
+            return _context.Projects.ToList();
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var projects = _context.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+
+            return projects;
+        }
+    }
+}
---
+++ backend/WaterProject/WaterProject/Data/WaterDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WaterProject.Data
+{
+    public class WaterDbContext : DbContext
+    {
+        public WaterDbContext(DbContextOptions<WaterDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
