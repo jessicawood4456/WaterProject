43f427b5f3af9f4267f0959b60c060c8b3408531
Jessica Lasson <jessicawood4456@gmail.com>
2025-03-22 20:03:53 -0600

    Added sorting by project category types.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import './App.css';
+import CategoryFilter from './CategoryFilter';
+// import Fingerprint from './Fingerprint';
 import ProjectList from './ProjectList';
+import WelcomeBand from './WelcomeBand';
+// import CookieConsent from 'react-cookie-consent';
 
 function App() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
   return (
     <>
-      <ProjectList />
+      <div className="container mt-4">
+        <div className="row bg-primary text-white">
+          <WelcomeBand />
+          <br />
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <CategoryFilter
+              selectedCategories={selectedCategories}
+              setSelectedCategories={setSelectedCategories}
+            />
+          </div>
+          <div className="col-md-9">
+            <ProjectList selectedCategories={selectedCategories} />
+          </div>
+        </div>
+      </div>
+
+      {/* <CookieConsent>
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
+      <Fingerprint /> */}
     </>
   );
 }
---
+++ frontend/src/WelcomeBand.tsx
@@ -0,0 +1,10 @@
+function WelcomeBand() {
+  return (
+    <>
+      <h1>Water Projects</h1>
+      <br />
+    </>
+  );
+}
+
+export default WelcomeBand;
